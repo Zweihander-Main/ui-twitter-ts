478dc680af475839657592163db29d02699aaf69
Zweihänder <zweidev@zweihander.me>
2020-04-18 22:02:47 -0400

    Added in codegen and custom faunadb loader

---
+++ scripts/faunadb-loader.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console, @typescript-eslint/no-var-requires, @typescript-eslint/explicit-function-return-type */
+const {
+	getIntrospectionQuery,
+	buildClientSchema,
+} = require('graphql/utilities');
+const fetch = require('node-fetch');
+
+module.exports = (url) => {
+	const options = {
+		method: 'POST',
+		headers: {
+			Authorization: `Bearer ${process.env.FAUNADB_SERVER_SECRET}`,
+		},
+	};
+	return fetch(url, {
+		method: options.method,
+		headers: options.headers,
+		body: JSON.stringify({ query: getIntrospectionQuery() }),
+	})
+		.then((res) => res.json())
+		.then((schemaJSON) => buildClientSchema(schemaJSON.data));
+};
